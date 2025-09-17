a3c878cd415f9ed6cc994638142ecbb374b572c2
KyathiVardhan-Lex <kyathi.konkeni@lexfintech.io>
2025-09-17 15:23:01 +0530

    third changes

--- backend/user/src/index.ts
+++ backend/user/src/index.ts
@@ -4,6 +4,7 @@ import connectDB from './config/db.js';
 import { createClient } from 'redis';
 import userRoutes from './routes/user.js'
 import { connectRabbitMQ } from './config/rabbitmq.js';
+import cors from 'cors'
 
 dotenv.config();
 
@@ -20,11 +21,11 @@ export const redisClient = createClient({
 redisClient.connect().then(()=> console.log("connected to redis")) .catch(console.error);
 
 const app = express();
-app.use(express.json())
+app.use(express.json());
+
+app.use(cors());
+
 app.use("/api/v1", userRoutes);
-app.use("/api/v1", userRoutes)
-
-
 
 const port = process.env.PORT || 5000;
 
