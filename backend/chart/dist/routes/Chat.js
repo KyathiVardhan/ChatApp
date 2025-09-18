880047140aa9a826701746d7e0d3becac59cafbb
KyathiVardhan-Lex <kyathi.konkeni@lexfintech.io>
2025-09-18 19:49:30 +0530

    chat updated

--- backend/chart/dist/routes/Chat.js
+++ backend/chart/dist/routes/Chat.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import { isAuth } from '../middleware/isAuth.js';
+import { createNewChat } from '../controller/chat.js';
 const router = express.Router();
+router.post('/chat/new', isAuth, createNewChat);
 export default router;
 //# sourceMappingURL=Chat.js.map
